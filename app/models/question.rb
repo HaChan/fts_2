f5899d3e65ed7f51bb184a9a36a4db0917ad29d8
HaChan <hades.24791@gmail.com>
2014-01-15 09:14:25 +0700

    ref #taskListAllQuestionAndDeleteQuestion

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,6 @@
 class Question < ActiveRecord::Base
   belongs_to :subject
+  belongs_to :type_question
   has_many :answers
   accepts_nested_attributes_for :answers,
   reject_if: lambda { |params| params[:answer_content].blank? }, allow_destroy: true
